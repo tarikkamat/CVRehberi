2f6b31c616cb729848aece39dd349051a138c982
Tarık KAMAT <tarikkamat2@gmail.com>
2023-05-04 00:58:28 +0300

    add http

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 use Spatie\PdfToText\Pdf;
 
 class FileController extends Controller
