b00ccc61cd4e70b73d2bd64aed92cc65b51be941
Tarık KAMAT <tarikkamat2@gmail.com>
2023-05-04 00:55:35 +0300

    final version

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,13 +10,13 @@ class FileController extends Controller
 {
     public function upload(Request $request)
     {
-        $validate = $request->validate([
+        $request->validate([
             'file' => 'required|mimes:pdf|max:2048',
             'job' => 'string|max:50'
         ]);
 
-        $text = Pdf::getText($validate['file']);
-        $job = $request->$validate['job'];
+        $text = Pdf::getText($request->file);
+        $job = $request->job;
 
         $suggestion = "Mesleğim: $job\n\n Özgeçmişim: $text\n\n Özgeçmişimi analiz edip eksiklerimi ve hatalarımı maddeler halinde paylaşır mısın?";
 
