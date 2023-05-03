06343b1ed670872caf218da4d8ca87a63a74b28c
Tarık KAMAT <tarikkamat2@gmail.com>
2023-05-04 00:57:40 +0300

    test

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -18,9 +18,30 @@ class FileController extends Controller
         $text = Pdf::getText($request->file);
         $job = $request->job;
 
-        $suggestion = "Mesleğim: $job\n\n Özgeçmişim: $text\n\n Özgeçmişimi analiz edip eksiklerimi ve hatalarımı maddeler halinde paylaşır mısın?";
+        $search = "Mesleğim: $job\n\n Özgeçmişim: $text\n\n Özgeçmişimi analiz edip eksiklerimi ve hatalarımı maddeler halinde paylaşır mısın?";
 
-        return dd($suggestion);
+        $data = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
+        ])
+            ->post("https://api.openai.com/v1/chat/completions", [
+                "model" => "gpt-3.5-turbo",
+                'messages' => [
+                    [
+                        "role" => "user",
+                        "content" => $search
+                    ]
+                ],
+                'temperature' => 0.5,
+                "max_tokens" => 200,
+                "top_p" => 1.0,
+                "frequency_penalty" => 0.52,
+                "presence_penalty" => 0.5,
+                "stop" => ["11."],
+            ])
+            ->json();
+
+        return response()->json($data['choices'][0]['message'], 200, array(), JSON_PRETTY_PRINT);
 
     }
 }
